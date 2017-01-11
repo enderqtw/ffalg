e8a2c670b35e8f721bb66c40326ae4de46f6479b
enderqtw <enderqtw@gmail.com>
2017-01-11 16:16:34 +0200

    Add files via upload

--- Percolation.java
+++ Percolation.java
@@ -1,85 +1,119 @@
-package Percolation;
-
+package algsTaks1;
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
+
 /**
- * Created by lolnyan on 10.01.2017.
+ * Created by enderqtw on 10.01.2017.
  */
 public class Percolation {
-    public WeightedQuickUnionUF quickUnion;
-    public int[][] grid;
-    public int n;
-    public Percolation(int n){
-        if(n <= 0)
-            throw new java.lang.IllegalArgumentException("The number must be >0!");
+    private WeightedQuickUnionUF quickUnion;
+    private int[][] grid;
+    private int n;
+
+    public Percolation(int n) {
+        if (n <= 0) {
+            throw new IndexOutOfBoundsException("row index i out of bounds");
+        }
+
         this.n = n;
-        quickUnion = new WeidhtedQuickUnionUF(n * n + 2);
-        for(int i = 1; i <= n; i++)
-            quickUnion.union(0, i);
-        for(int i = n * n; i > n * n - n; i++)
-            quickUnion.union(n * n + 1, i);
-        grid = new int[n][n];
-    }   //grid creation method
+        quickUnion = new WeightedQuickUnionUF(n * n + 1);
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                quickUnion.union(i, j);
+            }
+        }
 
-    public void open(int row, int col){
-        if((row > n) || (row < 1) || (col > n) || (col < 1))
-            throw new java.lang.IllegalArgumentException("The number is incorrect!");
-        if(grid[row-1][col-1] == 1)
-            return;
-        grid[row-1][col-1] = 1;
-        if(col - 2 >= 0)
-            if((isOpen(row, col-1)) && (!quickUnion.connected((row-1) * n + col, (row - 1) * n + (col - 1))))
-                quickUnion.union((row - 1) * n + col, (row - 1) * n + (col - 1));
-        //check of the first element
-        if(col < n)
-            if((isOpen(row, col+1)) && (!quickUnion.connected((row - 1) * n + col, (row - 1) * n + (col - 1))))
-                quickUnion.union((row - 1) * n + col, (row - 1) * n + col + 1);
-        //check of the last element
-        if(row - 2 >= 0)
-            if((isOpen(row - 1, col)) && (!quickUnion.connected((row - 1) * n + col, (row - 2) * n + col)))
-                quickUnion.union((row - 1) * n + col, (row - 2) * n + col);
-        if(row < n)
-            if((isOpen(row + 1, col)) && (!quickUnion.connected((row - 1) * n + col, row * n + col)))
-                quickUnion.union((row - 1) * n + col, row * n + col);
-    }   //opens (row, col) site
+        grid = new int[n + 1][n + 1];
+    }   // grid creation method
 
-    public boolean isOpen(int row, int col){
-        if((row > n) || (row < 1) || (col < 1) || (col > n))
-            throw new java.lang.IllegalArgumentException("The number is incorrect!");
-        if(grid[row-1][col-1] == 1)
-            return true;
-        return false;
-    }   //returns boolean value weather site is open
+    public void open(int row, int col) {
+        if ((row > n) || (row < 1) || (col > n) || (col < 1)) {
+            throw new IndexOutOfBoundsException("row index i out of bounds");
+        }
 
-    public boolean isFull(int row, int col){
-        if((row > n) || (row < 1) || (col < 1) || (col > n))
-            throw new java.lang.IllegalArgumentException("The number is incorrect!");
-        if(isOpen(row, col))
-            if(quickUnion.connected((row - 1) * n + col, 0))
-                return true;
-        return false;
-    }   //returns boolean value weather site if full
+//        if (grid[row][col] == 1) {
+//            return;
+//        }
+        grid[row][col] = 1;
+        if (col - 1 > 0) {
+            if (isOpen(row, col - 1) && (!quickUnion.connected(viewParse(row, col) - 1, viewParse(row, col)))) {
+                quickUnion.union(viewParse(row, col) - 1, viewParse(row, col));
+            }
+        }
+        // check of the left element
+        if (col < n) {
+            if (isOpen(row, col + 1) && (!quickUnion.connected(viewParse(row, col) + 1, viewParse(row, col)))) {
+                quickUnion.union(viewParse(row, col) + 1, viewParse(row, col));
+            }
+        }
+        // check of the right element
+        if (row - 1 > 0) {
+            if (isOpen(row - 1, col) && (!quickUnion.connected(viewParse(row, col) - n, viewParse(row, col)))) {
+                quickUnion.union(viewParse(row, col) - n, viewParse(row, col));
+            }
+        }
+        // check of the top element
+        if (row < n) {
+            if (isOpen(row + 1, col) && (!quickUnion.connected(viewParse(row, col) + n, viewParse(row, col)))) {
+                quickUnion.union(viewParse(row, col) + n, viewParse(row, col));
+            }
+        }
+        // check of the bottom element
+    }   // opens (row, col) site
 
-    public int numberOfOpenSites(){
-        int count = 0;
-        for(int i = 1; i <= n; i++)
-            for(int j = 1; j <= n; j++) //grid[0].
-                if(isOpen(i, j))
-                    count++;
-        return count;
-    }   //returns integer value of open sites
 
-    public boolean percolates(){
-        if(quickUnion.connected(0, n * n + 1)) {
-            if (n == 1)
-                if (!isOpen(1, 1))
-                    return false;
+    private int viewParse(int row, int col) {
+        return (row - 1) * n + col;
+    }
+
+    public boolean isOpen(int row, int col) {
+        if ((row > n) || (row < 1) || (col < 1) || (col > n)) {
+            throw new IndexOutOfBoundsException("row index i out of bounds");
+        }
+
+        if (grid[row][col] == 1) {
             return true;
         }
         return false;
-    }   //returns boolean value weather grid percolates
+    }   // returns boolean value weather site is open
 
-    public static void main(String[] args){
-        Percolation perc = new Percolation(1);
-        System.out.println(perc.percolates());
-    }   //optional
+    public boolean isFull(int row, int col) {
+        if ((row > n) || (row < 1) || (col < 1) || (col > n)) {
+            throw new IndexOutOfBoundsException("row index i out of bounds");
+        }
+
+        if (isOpen(row, col)) {
+            if (quickUnion.connected((row - 1) * n + col, 0)) {
+                return true;
+            }
+        }
+        return false;
+    }   // returns boolean value weather site if full
+
+    public int numberOfOpenSites() {
+        int count = 0;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (isOpen(i, j)) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }   // returns integer value of open sites
+
+    public boolean percolates() {
+        if (quickUnion.connected(1, n * n)) {
+            if (n == 1) {
+                if (!isOpen(1, 1)) {
+                    return false;
+                }
+            }
+            return true;
+        }
+        return false;
+    }   // returns boolean value weather grid percolates
+
+    public static void main(String[] args) {
+
+    }   // optional
 }
--- PercolationStats.java
+++ PercolationStats.java
@@ -1,53 +1,59 @@
-package Percolation;
-
+package algsTaks1;
+import edu.princeton.cs.algs4.StdIn;
 import edu.princeton.cs.algs4.StdRandom;
 import edu.princeton.cs.algs4.StdStats;
-/**
- * Created by lolnyan on 10.01.2017.
- */
-public class PercolationStats {
-    public Percolation[] perc;
-    public int[] stats;
-    public int trials;
 
-    public PercolationStats(int n, int trials){
-        if((n < 1) || (trials < 1))
-            throw new java.lang.IllegalArgumentException("The number is incorrect!");
+
+    /**
+    *   Created by enderqtw on 10.01.2017.
+    */
+public class PercolationStats {
+    private Percolation[] perc;
+    private int[] stats;
+    private int trials;
+
+    public PercolationStats(int n, int trials) {
+        if ((n < 1) || (trials < 1)) {
+            throw new IndexOutOfBoundsException("row index i out of bounds");
+        }
         this.trials = trials;
-        perc = new Percolation[trials];
-        stats = new int[trials];
-        for(int i = 0; i < trials; i++)
+        perc = new Percolation[trials + 1];
+        stats = new int[trials + 1];
+        for (int i = 1; i <= trials; i++) {
             perc[i] = new Percolation(n);
+        }
     }
 
-    public double mean(){
+    public double mean() {
         return StdStats.mean(stats);
     }
 
-    public double stddev(){
+    public double stddev() {
         return StdStats.stddev(stats);
     }
 
-    public double confidenceLo(){
+    public double confidenceLo() {
         return mean() - (1.96 * stddev() / Math.sqrt(trials));
     }
-    public double confidenceHi(){
+
+    public double confidenceHi() {
         return mean() + (1.96 * stddev() / Math.sqrt(trials));
     }
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         PercolationStats pcStats = new PercolationStats(Integer.parseInt(args[0]), Integer.parseInt(args[1]));
-        for(int i = 0; i < pcStats.trials; i++){
-            while(!pcStats.perc[i].percolates())
+
+        for (int i = 1; i <= Integer.parseInt(args[1]); i++) {
+            while (!pcStats.perc[i].percolates()) {
                 pcStats.perc[i].open(StdRandom.uniform(Integer.parseInt(args[0])) + 1, StdRandom.uniform(Integer.parseInt(args[0])) + 1);
+            }
             pcStats.stats[i] = pcStats.perc[i].numberOfOpenSites();
             System.out.println(pcStats.stats[i]);
         }
+
         System.out.println("mean = " + pcStats.mean());
         System.out.println("stddev = " + pcStats.stddev());
         System.out.println("95% confidence interval = " + pcStats.confidenceLo() + ", " + pcStats.confidenceHi());
-        System.out.println();
-
     }
 
 }
