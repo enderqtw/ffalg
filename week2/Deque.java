59d2e112c18be3a43f9fb16c88405874ce123315
enderqtw <enderqtw@gmail.com>
2017-01-16 23:37:35 +0200

    Add files via upload

--- week2/Deque.java
+++ week2/Deque.java
@@ -1,58 +1,110 @@
 package week2;
 
 import java.util.Iterator;
-import java.util.LinkedList;
-import java.util.ArrayList;
 import java.util.NoSuchElementException;
 
 /**
- * Created by woice on 17.16.1.
+ * Created by woice on 17.01.2017.
  */
 public class Deque<Item> implements Iterable<Item> {
+    private Item[] queue;
 
-    private Deque first = null, last = null;
-    private class Node{}
     public Deque() {
-
-        Item item;
-        Deque next;
-    }   //  construct an empty deque
+        queue = (Item[]) new Object[0];
+    }   // constructor of Deque
 
     public boolean isEmpty() {
-        return first == null;
-    }   //  return boolean value weather is empty
+        if (queue.length != 0) {
+            return false;
+        }
+        return true;
+    }   //  is the deque empty?
 
     public int size() {
-        return first.size();
-    }   //  return integer value of deque size
+        return queue.length;
+    }   //  return the number of items on the deque
 
     public void addFirst(Item item) {
-        Deque oldfirst = first;
-        first = new Deque();
-        first.item = item;
-        first.next = oldfirst;
-    }   //  add to deque the first element
+        if (item == null) {
+            throw new NullPointerException("Cannot add a null element");
+        }
+        Item[] deque = (Item[]) new Object[queue.length + 1];
+        for (int i = 0; i < queue.length; i++) {
+            deque[i] = queue[i];
+        }
+        queue = deque;
+        queue[queue.length - 1] = item;
+
+    }   //  add the item to the front
 
     public void addLast(Item item) {
-
-    }      //   add to deque the last element
+        if (item == null) {
+            throw new NullPointerException("Cannot add a null element");
+        }
+        Item[] deque = (Item[]) new Object[queue.length + 1];
+        for (int i = 1; i < deque.length; i++) {
+            deque[i] = queue[i - 1];
+        }
+        queue = deque;
+        queue[0] = item;
+    }   //  add the item to the end
 
     public Item removeFirst() {
-        first = null;
-        return null;
-    }   //  remove from deque current first element
+        if (isEmpty()) {
+            throw new NoSuchElementException("Is empty");
+        }
+        Item removeNumber = queue[queue.length - 1];
+        Item[] deque = (Item[]) new Object[queue.length - 1];
+        for (int i = 0; i < deque.length; i++) {
+            deque[i] = queue[i];
+        }
+        queue = deque;
+        return removeNumber;
+    }   //  remove and return the item from the front
 
     public Item removeLast() {
-        last = null;
-        return null;
-    }   //  remove from deque current last element
+        if (isEmpty()) {
+            throw new NoSuchElementException("Is empty");
+        }
+        Item removeNumber = queue[0];
+        Item[] deque = (Item[]) new Object[queue.length - 1];
+        for (int i = 0; i < deque.length; i++) {
+            deque[i] = queue[i + 1];
+        }
+        queue = deque;
+        return removeNumber;
+    }   //  remove and return the item from the end
 
-    public Iterator<Item> iterator(){}; //  iterator constructor
+    private class ArrayIterator implements Iterator<Item> {
+        private int k = queue.length - 1;
 
-    private void resize(int capacity){
+        @Override
+        public boolean hasNext() {
+            if (k >= 0) {
+                return true;
+            }
+            return false;
+        }
 
+        @Override
+        public Item next() {
+            if (!hasNext()) {
+                throw new NoSuchElementException("Doesn't have a next element");
+            }
+            Item nextElement = queue[k--];
+            return nextElement;
+        }
+
+        @Override
+        public void remove() {
+            throw new UnsupportedOperationException("Cannot remove");
+        }
     }
-    public static void main(String[] args) {
 
+    public Iterator<Item> iterator() {
+        return new ArrayIterator();
+    }   //  return an iterator over items in order from front to end
+
+    public static void main(String[] args) {
     }   //  optional
 }
